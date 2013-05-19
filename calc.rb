b70d6dd16eef48478e4a0bfbd973f34a5e5b1b93
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-05-19 12:35:19 -0400

    formatting

--- calc.rb
+++ calc.rb
@@ -3,78 +3,79 @@
 
 require 'optparse'
 require 'logger'
-require 'linear_regression_calculator'
+require_relative 'linear_regression_calculator'
 
 $logger = Logger.new(STDOUT)
 $logger.level = Logger::INFO
 
 def usage
-  'Usage: lin_reg_calc.rb --train FILE --input FILE [--rate FLOAT] [--delimiter CHAR]'
+  'Usage: lin_reg_calc.rb --train FILE --input FILE '\
+  '[--rate FLOAT] [--delimiter CHAR]'
 end
 
 def parse_options
 
   options = {}
   optparse = OptionParser.new do |opts|
-  
+
     opts.banner = usage
-    
+
     options[:training_file] = nil
     opts.on('-t', '--train FILE', 'File with training data') do |file|
       options[:training_file] = file
     end
-    
+
     options[:input_file] = nil
-    opts.on('-i', '--input FILE', 'Input file with required predictions') do |file|
+    opts.on('-i', '--input FILE', 'Input file with required '\
+            'predictions') do |file|
       options[:input_file] = file
     end
-    
+
     options[:rate] = 0.1
     opts.on('-r', '--rate FLOAT', 'Learning rate (default is 0.1)') do |num|
       options[:rate] = num.to_f
     end
-    
+
     options[:delimiter] = ','
-    opts.on('-l', '--delimiter CHAR', 'Character delimiting the data (default is \',\')') do |char|
+    opts.on('-l', '--delimiter CHAR', 'Character delimiting the data '\
+            '(default is \',\')') do |char|
       options[:delimiter] = char
     end
-        
+
     opts.on('-d', '--debug', 'Print debugging output') do
       $logger.level = Logger::DEBUG
     end
-    
+
     opts.on('-h', '--help', 'Display this screen') do
      puts opts
      exit
     end
-    
+
   end
-  
+
   optparse.parse!
   return options
-  
+
 end
 
 def main
-  
+
   options = parse_options
 
-  if options[:training_file] and options[:input_file]
-    
+  if options[:training_file] && options[:input_file]
+
     start_time = Time.now
-    
+
     calc = LinearRegressionCalculator.new(options[:rate])
-    
     calc.learn_data(options[:training_file], options[:delimiter])
-    
     calc.predict(options[:input_file], options[:delimiter])
-    
+
     end_time = Time.now
-    
-    $logger.info('Total time=%ds' %[end_time - start_time])
-    
+
+    $logger.info(sprintf('Total time=%ds', end_time - start_time))
+
   else
-  
+
     puts usage
     exit
 
@@ -82,6 +83,4 @@ def main
 
 end
 
-if __FILE__ == $0
-  main
-end
+main if __FILE__ == $PROGRAM_NAME
--- linear_regression_calculator.rb
+++ linear_regression_calculator.rb
@@ -4,27 +4,27 @@ class LinearRegressionCalculator
     @learning_rate = learning_rate
   end
 
-  def learn_data(file,delimiter)
+  def learn_data(file, delimiter)
   
-    maxs,mins,avgs = get_normalization_params(file,delimiter)
-    @normalize = lambda { |feature,i| (feature - avgs[i]) / (maxs[i] - mins[i]) }
-    @theta = gradient_descent_algorithm(file,delimiter,maxs.size)
+    maxs, mins, avgs = get_normalization_params(file, delimiter)
+    @normalize = ->(feature, i) { (feature - avgs[i]) / (maxs[i] - mins[i]) }
+    @theta = gradient_descent_algorithm(file, delimiter, maxs.size)
 
   end
   
-  def predict(file,delimiter)
+  def predict(file, delimiter)
   
-    line_count = %x{wc -l #{file}}.split.first.to_i
+    line_count = %x(wc -l #{file}).split.first.to_i
     line_num = 0
     File.open(file + '.out', 'w') do |file_handle|
       File.open(file, 'r').each_line do |line|
         line_num += 1
-        $logger.info('Processing line %d of %d' %[line_num,line_count])
+        $logger.info(sprintf('Processing line %d of %d', line_num, line_count))
         tokens = line.chomp.split(delimiter)
         $logger.debug(tokens)
         sum = @theta[0]
         (0...(@theta.size - 1)).each do |i|
-          sum += (@theta[i+1] * @normalize.call(tokens[i].to_f,i))
+          sum += (@theta[i + 1] * @normalize.call(tokens[i].to_f, i))
         end
         file_handle.write(tokens.join(delimiter) + delimiter + sum.to_s)
         file_handle.write("\n")
@@ -35,9 +35,9 @@ class LinearRegressionCalculator
   
   private
   
-  def gradient_descent_algorithm(file,delimiter,num_features)
+  def gradient_descent_algorithm(file, delimiter, num_features)
   
-    line_count = %x{wc -l #{file}}.split.first.to_i
+    line_count = %x(wc -l #{file}).split.first.to_i
     line_num = 0
     
     x = []
@@ -61,31 +61,32 @@ class LinearRegressionCalculator
       new_theta = []
       File.open(file, 'r').each_line do |line|
         line_num += 1
-        $logger.debug('Processing line %d of %d' %[line_num,line_count])
+        $logger.debug(sprintf('Processing line %d of %d', line_num, line_count))
         tokens = line.chomp.split(delimiter)
         $logger.debug(tokens)
         arr = []
         (0...num_features).each do |i|
-          arr << @normalize.call(tokens[i].to_f,i)
+          arr << @normalize.call(tokens[i].to_f, i)
         end
         x = [1.0] + arr
         $logger.debug(x)
         y = tokens.last.to_f
         $logger.debug(y)
-        h = dot_prod(theta,x)
+        h = dot_prod(theta, x)
         error = h - y
-        j += (error ** 2)
+        j += (error**2)
         (0...x.size).each do |i|
           new_theta[i] ||= 0.0
           new_theta[i] += (error * x[i])
         end
       end
       
-      j /= (2*line_num)
+      j /= (2 * line_num)
       $logger.debug(prev_j)
       $logger.info(j)
       (0...new_theta.size).each do |index|
-        new_theta[index] = theta[index] - ((@learning_rate * new_theta[index]) / line_num)
+        new_theta[index] = theta[index] - 
+                           ((@learning_rate * new_theta[index]) / line_num)
       end
       
       break unless j < prev_j
@@ -94,13 +95,13 @@ class LinearRegressionCalculator
     theta
   end
   
-  def dot_prod(vect1,vect2)
-    (0...vect1.count).inject(0.0) { |acc, index| acc + vect1[index]*vect2[index] }
+  def dot_prod(vect1, vect2)
+    (0...vect1.count).reduce(0.0) { |acc, index| acc + (vect1[index] * vect2[index]) }
   end
   
-  def get_normalization_params(file,delimiter)
+  def get_normalization_params(file, delimiter)
   
-    line_count = %x{wc -l #{file}}.split.first.to_i
+    line_count = %x(wc -l #{file}).split.first.to_i
     line_num = 0
     maxs = []
     mins = []
@@ -108,7 +109,7 @@ class LinearRegressionCalculator
     x_dim = 0
     File.open(file, 'r').each_line do |line|
       line_num += 1
-      $logger.debug('Processing line %d of %d' %[line_num,line_count])
+      $logger.debug(sprintf('Processing line %d of %d', line_num, line_count))
       tokens = line.chomp.split(delimiter)
       $logger.debug(tokens)
       if line_num == 1
@@ -131,7 +132,7 @@ class LinearRegressionCalculator
     $logger.debug(mins)
     $logger.debug(avgs)
   
-    return maxs,mins,avgs
+    return maxs, mins, avgs
   end
   
 end
